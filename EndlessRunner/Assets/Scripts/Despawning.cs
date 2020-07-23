5694e043c12b82b5456562d6f4574091545ed497
18020967 <56230874+18020967@users.noreply.github.com>
2020-07-23 16:00:42 +0200

    problem
    reverting

--- EndlessRunner/Assets/Scripts/Despawning.cs
+++ EndlessRunner/Assets/Scripts/Despawning.cs
@@ -4,17 +4,36 @@ public class Despawning : MonoBehaviour
 {
 
     PlatformManager platform;
-    private void OnCollisionStay(Collision collision)
-    {
-        if (collision.gameObject.tag == "Respawn")
-        {
-            //Debug.Log("Collided with : " + collision.gameObject.tag);
+    //private void OnCollisionStay(Collision collision)
+    //{
+    //    if (collision.gameObject.tag == "Respawn")
+    //    {
+    //        //Debug.Log("Collided with : " + collision.gameObject.tag);
             
-            Destroy(gameObject);
+    //        Destroy(gameObject);
 
-            GameObject go = GameObject.Find("PlatformManagerObject");
-            PlatformManager platform =(PlatformManager) go.GetComponent(typeof(PlatformManager));
-            platform.PlacePlatform();
-        }
-    }
+    //        GameObject go = GameObject.Find("PlatformManagerObject");
+    //        PlatformManager platform =(PlatformManager) go.GetComponent(typeof(PlatformManager));
+    //        platform.PlacePlatform();
+    //    }
+    //}
+
+
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Platform")
+		{
+			Debug.Log("IT WORKS!"+gameObject+"  "+ other.gameObject);
+			
+			Destroy(other.gameObject);
+
+			GameObject go = GameObject.Find("PlatformManagerObject");
+			PlatformManager platform = (PlatformManager)go.GetComponent(typeof(PlatformManager));
+			platform.PlacePlatform();
+
+		}
+	}
+
+
 }
--- EndlessRunner/Assets/Scripts/PlatformManager.cs
+++ EndlessRunner/Assets/Scripts/PlatformManager.cs
@@ -2,13 +2,21 @@
 
 public class PlatformManager : MonoBehaviour
 {
-    [SerializeField]
+	public Transform player;
+	int obstacles;
+	bool boss = false;
+
+	[SerializeField]
     private GameObject[] Platforms;
-    float PlatLength = 9;
+
+	[SerializeField]
+	private GameObject[] BossPlatforms;
+
+	float PlatLength = 9;
 
     void Start()
     {
-        for (int i = 1; i < 40; i++)
+        for (int i = 1; i < 10; i++)
         {
             PlacePlatform();
         } 
@@ -16,11 +24,30 @@ public class PlatformManager : MonoBehaviour
 
 
     int randomNum;
-    public void PlacePlatform()
-    {
-        randomNum = Random.Range(0,Platforms.Length);
-        //Debug.Log(randomNum);
-        Instantiate(Platforms[randomNum], new Vector3(PlatLength, 0, 0),Quaternion.identity);
-        PlatLength += 18;
+	int bossRanNum;
+	public void PlacePlatform()
+	{
+		if (boss == false)
+		{ 
+			randomNum = Random.Range(0, Platforms.Length);
+			Instantiate(Platforms[randomNum], new Vector3(PlatLength, 0, 0), Quaternion.identity);
+			PlatLength += 18;
+		}
+		else
+		{
+			bossRanNum = Random.Range(0, BossPlatforms.Length);
+			Instantiate(BossPlatforms[bossRanNum], new Vector3(PlatLength, 0, 0), Quaternion.identity);
+			PlatLength += 18;
+		}
     }
+
+	private void Update()
+	{
+		obstacles = player.gameObject.GetComponent<PlayerCollision>().obstaclePassed;
+
+		if (obstacles >= 10)
+		{
+			boss = true;
+		}
+	}
 }
